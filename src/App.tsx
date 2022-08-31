7afd8a2322358b27d395b78cc9c8ca70db5c6c78
OKTheWorld <oktheworld@au.com>
2022-09-01 01:22:06 +0900

    scp 実行テスト 2回目

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CI scp
+          Learn React CI scp2
         </a>
       </header>
     </div>
