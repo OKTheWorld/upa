ea13a131c19906de387367785efcc0ca36375ce6
OKTheWorld <oktheworld@au.com>
2022-09-01 01:36:56 +0900

    デプロイ実行テスト 2回目

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CICD
+          Learn React
         </a>
       </header>
     </div>
